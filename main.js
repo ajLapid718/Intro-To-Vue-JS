bc8cc2db2b9b3abc6b448f7ddc249eef1dc89d31
Allan James Lapid <ajLapid718@gmail.com>
2020-06-26 16:39:53 -0400

    add image property to vue instance

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const app = new Vue({
   el: "#app",
   data: {
     product: "Socks",
-    description: "One can never have enough of these."
+    description: "One can never have enough of these.",
+    image: "./assets/vmSocks-green.jpg"
   }
 });
