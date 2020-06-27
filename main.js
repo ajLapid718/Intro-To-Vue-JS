9708d579131c1b9cade1fb25c9c603c2ab83b87c
Allan James Lapid <ajLapid718@gmail.com>
2020-06-27 03:54:36 -0400

    add sizes array to data object

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const app = new Vue({
     inStock: false,
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [{ variantId: 2234, variantColor: "green"}, { variantId: 2235, variantColor: "blue"}]
+    variants: [{ variantId: 2234, variantColor: "green"}, { variantId: 2235, variantColor: "blue"}],
+    sizes: ["Small", "Medium", "Large"]
   }
 });
