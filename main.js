3562c38acc7c3e2b21c5afd1948d1b9125370d6a
Allan James Lapid <ajLapid718@gmail.com>
2020-06-28 02:37:05 -0400

    return data object

--- main.js
+++ main.js
@@ -44,7 +44,24 @@ Vue.component("product", {
         <a v-bind:href="link">Click Here For More Information</a>
       </div>
   </div>`,
-  
+  data: function () {
+    return {
+      product: "Socks",
+      brand: "Vue Mastery",
+      description: "One can never have enough of these.",
+      selectedVariant: 0,
+      // image: "./assets/vmSocks-green.jpg",
+      link: "https://www.google.com/",
+      // inStock: false,
+      // onSale: false,
+      // inventory: 0,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [{ variantId: 2234, variantColor: "green", variantImage: "./assets/vmSocks-green.jpg", variantQuantity: 100}, { variantId: 2235, variantColor: "blue", variantImage: "./assets/vmSocks-blue.jpg", variantQuantity: 0}],
+      sizes: ["Small", "Medium", "Large"],
+      onSale: true,
+      cart: 0
+    }
+  }
 })
 
 const app = new Vue({
