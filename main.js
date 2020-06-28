9b488d0195c3535aad1cb2e65ab42622f0199d45
Allan James Lapid <ajLapid718@gmail.com>
2020-06-28 02:38:23 -0400

    port over methods and computed properties into component

--- main.js
+++ main.js
@@ -61,26 +61,6 @@ Vue.component("product", {
       onSale: true,
       cart: 0
     }
-  }
-})
-
-const app = new Vue({
-  el: "#app",
-  data: {
-    product: "Socks",
-    brand: "Vue Mastery",
-    description: "One can never have enough of these.",
-    selectedVariant: 0,
-    // image: "./assets/vmSocks-green.jpg",
-    link: "https://www.google.com/",
-    // inStock: false,
-    // onSale: false,
-    // inventory: 0,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [{ variantId: 2234, variantColor: "green", variantImage: "./assets/vmSocks-green.jpg", variantQuantity: 100}, { variantId: 2235, variantColor: "blue", variantImage: "./assets/vmSocks-blue.jpg", variantQuantity: 0}],
-    sizes: ["Small", "Medium", "Large"],
-    onSale: true,
-    cart: 0
   },
   methods: {
     addToCart: function () {
@@ -113,4 +93,8 @@ const app = new Vue({
       }
     }
   }
+})
+
+const app = new Vue({
+  el: "#app"
 });
