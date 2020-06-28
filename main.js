aca1f0251a6c6e16056008e01f71bf6811c5d88c
Allan James Lapid <ajLapid718@gmail.com>
2020-06-28 02:34:36 -0400

    port over template

--- main.js
+++ main.js
@@ -1,3 +1,52 @@
+Vue.component("product", {
+  template: `<div class="product">
+    <div class="product-image">
+        <img v-bind:src="image">
+      </div>
+
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
+        <p>{{ sale }}</p>
+        <p v-show="inStock">In Stock</p>
+        <!-- <span v-if="onSale">On Sale!</span> -->
+        <!-- <p v-else-if="inventory <= 10 && inventory > 0">Almost Sold Out!</p> -->
+        <p v-show="!inStock" v-bind:class="{ 'out-of-stock': !inStock }">Out of Stock</p>
+        <ul>
+          <li v-for="detail in details" v-bind:key="detail">{{ detail }}</li>
+        </ul>
+
+        <div v-for="(variant, index) in variants" 
+            v-bind:key="variant.variantId"
+            class="color-box"
+            v-bind:style="{ backgroundColor: variant.variantColor }"
+            v-on:mouseover="updateProduct(index)">
+        </div>
+
+        <ul>
+          <li v-for="size in sizes" v-bind:key="size">{{ size }}</li>
+        </ul>
+
+        <!-- wherever you place the v-for directive, that will be the element that will be the element the loop will re-create -->
+
+        <div class="buttons-container">
+          <button v-on:click="addToCart"
+                  v-bind:disabled="!inStock"
+                  v-bind:class="{ disabledButton: !inStock }">
+                  Add To cart
+          </button>
+          <button @click="removeFromCart">Remove From Cart</button>
+          <button class="cart">Cart({{ cart }})</button>
+        </div>
+
+        <br>
+        <br>
+        <a v-bind:href="link">Click Here For More Information</a>
+      </div>
+  </div>`,
+  
+})
+
 const app = new Vue({
   el: "#app",
   data: {
