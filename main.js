7e7e6c1d0178ac9d9d0ed181ad92e60af0a1c031
Allan James Lapid <ajLapid718@gmail.com>
2020-06-27 22:43:40 -0400

    use v-on event listener to handle incrementing of cart upon click

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const app = new Vue({
     onSale: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
     variants: [{ variantId: 2234, variantColor: "green"}, { variantId: 2235, variantColor: "blue"}],
-    sizes: ["Small", "Medium", "Large"]
+    sizes: ["Small", "Medium", "Large"],
+    cart: 0
   }
 });
